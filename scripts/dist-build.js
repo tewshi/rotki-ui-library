af4d4a6314cdbdd084a29ff05dbee9b4ac2297b9
Joseph Ojoko <entewshi@gmail.com>
2023-04-28 10:13:09 +0100

    more cleanups

--- scripts/dist-build.js
+++ scripts/dist-build.js
@@ -7,38 +7,26 @@ if (fs.existsSync(dist)) {
   fs.rmSync(dist, { recursive: true });
 }
 
-const tailwind = spawnSync('tailwindcss', ['-o dist/style.css --minify'], {
-  shell: true
-});
-if (tailwind.status) {
+const tailwind = spawnSync('pnpm', ['build:tailwind'], { shell: true });
+if (tailwind.status !== 0) {
   console.error('build:tailwind failed', tailwind.error);
-  // process.exit(1);
+  process.exit(1);
 }
-//
-const types = spawnSync(
-  'vue-tsc',
-  [
-    '-p tsconfig.build.json --declaration --emitDeclarationOnly --outDir dist/types'
-  ],
-  {
-    shell: true
-  }
-);
-if (types.status) {
+
+const types = spawnSync('pnpm', ['build:types'], { shell: true });
+if (types.status !== 0) {
   console.error('build:types failed', types.error);
-  // process.exit(1);
+  process.exit(1);
 }
-//
-spawnSync('pnpm', ['switch:v3']);
-const v3 = spawnSync('vite', ['build']);
-if (v3.status) {
+
+const v3 = spawnSync('pnpm', ['build:prod:v3'], { shell: true });
+if (v3.status !== 0) {
   console.error('build:v3 failed', v3.error);
   process.exit(1);
 }
-//
-spawnSync('pnpm', ['switch:v2']);
-const v2 = spawnSync('vite', ['build']);
-if (v2.status) {
+
+const v2 = spawnSync('pnpm', ['build:prod:v2'], { shell: true });
+if (v2.status !== 0) {
   console.error('build:v2 failed', v2.error);
   process.exit(1);
 }
