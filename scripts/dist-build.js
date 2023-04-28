a20a9625c903b0cca493f5719381d213caef0223
Joseph Ojoko <entewshi@gmail.com>
2023-04-28 18:35:12 +0100

    setup spawn correctly

--- scripts/dist-build.js
+++ scripts/dist-build.js
@@ -1,4 +1,4 @@
-const { spawnSync } = require('node:child_process');
+const { spawn } = require('node:child_process');
 const fs = require('node:fs');
 const path = require('node:path');
 const { isVue3, isVue2 } = require('vue-demi');
@@ -8,28 +8,46 @@ if (fs.existsSync(dist)) {
   fs.rmSync(dist, { recursive: true });
 }
 
-const tailwind = spawnSync('pnpm', ['build:tailwind'], { shell: true });
-if (tailwind.status !== 0) {
-  console.error('build:tailwind failed', tailwind.error);
-  process.exit(1);
-}
+const tailwind = spawn('pnpm', ['build:tailwind'], {
+  stdio: [process.stdout, process.stderr]
+});
 
-const types = spawnSync('pnpm', ['build:types'], { shell: true });
-if (types.status !== 0) {
-  console.error('build:types failed', types.error);
-  process.exit(1);
-}
+tailwind.on('close', code => {
+  if (code !== 0) {
+    console.error('build:tailwind failed', tailwind.error);
+    process.exit(code);
+  }
+});
+
+const types = spawn('pnpm', ['build:types'], {
+  stdio: [process.stdout, process.stderr]
+});
+types.on('close', code => {
+  if (code !== 0) {
+    console.error('build:types failed', types.error);
+    process.exit(code);
+  }
+});
 
 if (isVue3) {
-  const v3 = spawnSync('pnpm', ['build:prod:v3'], { shell: true });
-  if (v3.status !== 0) {
-    console.error('build:v3 failed', v3.error);
-    process.exit(1);
-  }
+  const v3 = spawn('pnpm', ['build:prod:v3'], {
+    stdio: [process.stdout, process.stderr]
+  });
+
+  v3.on('close', code => {
+    if (code !== 0) {
+      console.error('build:v3 failed', v3.error);
+      process.exit(code);
+    }
+  });
 } else if (isVue2) {
-  const v2 = spawnSync('pnpm', ['build:prod:v2'], { shell: true });
-  if (v2.status !== 0) {
-    console.error('build:v2 failed', v2.error);
-    process.exit(1);
-  }
+  const v2 = spawn('pnpm', ['build:prod:v2'], {
+    stdio: [process.stdout, process.stderr]
+  });
+  v2.on('close', code => {
+    if (code !== 0) {
+      console.error('build:v2 failed', v2.error);
+      process.exit(code);
+    }
+  });
 }
